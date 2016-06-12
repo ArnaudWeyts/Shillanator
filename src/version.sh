3f4db7d6834074673230f36586cab9cc5f544f58
Arnaud Weyts <arnaud.weyts@gmail.com>
2016-06-12 20:50:54 +0200

    new travis settings back at it again

--- src/version.sh
+++ src/version.sh
@@ -1,5 +1,5 @@
 #!/bin/bash -e
 # manages the versions of all the extensions
-VERSION="$(git name-rev --tags --name-only $(git rev-parse HEAD))"
+VERSION=$1
 perl -pi.back -e  "s/(?<=\"version\": \")(\S+)(?=\")/$VERSION/g" chrome/manifest.json
 perl -pi.back -e  "s/(?<=\"version\": \")(\S+)(?=\")/$VERSION/g" firefox/package.json
