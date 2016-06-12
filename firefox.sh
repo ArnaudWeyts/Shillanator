002d6da9be89c0cd8f0f83a3205b378516823178
Arnaud Weyts <arnaud.weyts@gmail.com>
2016-06-12 20:26:55 +0200

    modified script for beta and production releases

--- firefox.sh
+++ firefox.sh
@@ -1,4 +1,9 @@
 #!/bin/bash -e
 cd firefox
 npm install
-npm run xpimake
+VERSION="$(git name-rev --tags --name-only $(git rev-parse HEAD))"
+if [[ $VERSION == *"beta"* ]];then
+    npm run xpimake-beta
+else
+    npm run xpimake-prod
+fi
--- firefox/xpimake.sh
+++ firefox/xpimake.sh
@@ -1,5 +1,7 @@
 #!/bin/bash -e
 # this uses npm & jpm to make the xpi file
 jpm xpi
-jpm sign --api-key $FIREFOX_API --api-secret $FIREFOX_SECRET --xpi *.xpi
-rm @*.xpi
+if [[ $1 == "prod" ]]; then
+    jpm sign --api-key $FIREFOX_API --api-secret $FIREFOX_SECRET --xpi *.xpi
+    rm @*.xpi
+fi
