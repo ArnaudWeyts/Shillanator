90d52d70ba52a0be44cb2d3eded8972973f04659
Arnaud Weyts <arnaud.weyts@gmail.com>
2016-06-12 14:27:53 +0200

    fixed firefox keys

--- firefox/xpimake.sh
+++ firefox/xpimake.sh
@@ -1,4 +1,4 @@
 #!/bin/bash -e
 # this uses npm & jpm to make the xpi file
 jpm xpi
-jpm sign --api-key FIREFOX_API --api-secret FIREFOX_SECRET --xpi @shillanator-0.1.2.xpi
+jpm sign --api-key $FIREFOX_API --api-secret $FIREFOX_SECRET --xpi @shillanator-0.1.2.xpi
