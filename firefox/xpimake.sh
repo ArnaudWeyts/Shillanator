89d119bf8dd2b09b16103b6cef5e54dd9c9a0a74
Arnaud Weyts <arnaud.weyts@gmail.com>
2016-06-12 19:08:43 +0200

    fixed firefox xpi build script

--- firefox/xpimake.sh
+++ firefox/xpimake.sh
@@ -1,8 +1,5 @@
 #!/bin/bash -e
 # this uses npm & jpm to make the xpi file
-cd ..
-VERSION="$(git name-rev --tags --name-only $(git rev-parse HEAD))"
-cd firefox
 jpm xpi
-jpm sign --api-key $FIREFOX_API --api-secret $FIREFOX_SECRET --xpi @shillanator-$VERSION.xpi
+jpm sign --api-key $FIREFOX_API --api-secret $FIREFOX_SECRET --xpi *.xpi
 rm @*.xpi
