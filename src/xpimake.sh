6339779b38bd9e46d06f65eea8bd5573249945aa
Arnaud Weyts <arnaud.weyts@gmail.com>
2016-06-14 12:00:40 +0200

    improved xpimake script

--- src/xpimake.sh
+++ src/xpimake.sh
@@ -1,18 +1,38 @@
 #!/bin/bash -e
 # this uses npm & jpm to make the xpi file
-cd "$TRAVIS_BUILD_DIR"
+if test $# -ne 1; then
+  echo "Usage: xpimake.sh <add-on dir>"
+  exit 1
+fi
+
+dir=$1
+
 #gets the right version
 VERSION="$(git name-rev --tags --name-only $(git rev-parse HEAD))"
-cd "$TRAVIS_BUILD_DIR/firefox"
+
+cd "$dir"
+
 # installs dependencies
-npm install
+npm install jpm
+
 # set path so jpm can be used wihout npm
-export PATH="$TRAVIS_BUILD_DIR/firefox/node_modules/.bin/:$PATH"
+export PATH="$dir/node_modules/.bin/:$PATH"
+
+# make the xpi using jpm
 jpm xpi
+echo "Wrote unsigned xpi file"
+
 # if beta => don't sign
 if [[ $VERSION != *"beta"* ]];then
-    jpm sign --api-key $FIREFOX_API --api-secret $FIREFOX_SECRET --xpi *.xpi
-    rm @*.xpi
+    # this fixes build errors if the same version is being signed twice
+    if jpm sign --api-key $FIREFOX_API --api-secret $FIREFOX_SECRET --xpi *.xpi ; then
+        echo "Signed xpi file"
+        rm @*.xpi
+        echo "Removed unsigned xpi file"
+        # move the xpi file to the root directory
+        mv ./*.xpi ../firefox.xpi
+    else
+        echo "Signing failed, using unsigned file"
+        mv ./*xpi ../firefox-unsigned.xpi
+    fi
 fi
-# move the xpi file to the root directory
-mv ./shillanator*.xpi "$TRAVIS_BUILD_DIR/firefox.xpi"
